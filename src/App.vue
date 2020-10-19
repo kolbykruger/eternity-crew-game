2999ba9a41d8c2dbf848a57a338ab66ceb09cd0b
Kolby Kruger <kolbykruger@gmail.com>
2020-10-19 13:28:12 -0400

    completed minor structuring and v1.0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,163 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p class="game-name">Eternity Crew Game</p>
+
+    <div v-if="gameData">
+        <div v-if="stage < gameData.length">
+            <div class="game">
+                <QA :data="gameData[stage]" :stage="gameStage" @stage="showControls"/>
+            </div>
+
+            <div class="buttons" v-if="showNextButton">
+                <button class="button" @click="nextQA">Next question</button>
+            </div>
+        </div>
+        <div v-else>
+            <h2>Game Over</h2>
+            <p>There are no more questions left.</p>
+        </div>
+    </div>
+    <div v-else>
+        Loading game...
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import QA from '@/components/QA.vue'
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+      QA
+  },
+  data() {
+      return {
+          gameData: null,
+          gameStage: false,
+          stage: 0,
+          docid: '1Je5sf-knWA2OY2fQ1ETaLd7MvQG5S7AUG4VOkmG86tk',
+          showNextButton: false,
+      }
+  },
+  mounted() {
+      const $el = this;
+
+      fetch(`https://spreadsheets.google.com/feeds/cells/${this.docid}/od6/public/basic?alt=json`)
+      .then(response => response.json())
+      .then(data => {
+          let arr = data.feed.entry
+
+            //Remove the headers
+          arr.splice(0, 1)
+          arr.splice(0, 1)
+
+            //create a keyed array based on cell value for future matching
+            const keyedArray = []
+          arr.forEach((item) => {
+              const id = item.title.$t.split('')[1]
+              keyedArray.push({
+                  id: id - 2,
+                  text: item.content.$t
+              })
+          })
+
+            //create array of matched rows based on cell value
+          let matchedArray = []
+          for(let i = 0; i < keyedArray.length / 2; i++) {
+              let matches = keyedArray.filter(item => item.id == i)
+              let obj = {
+                  question: matches[0].text,
+                  answer: matches[1].text
+              }
+
+              matchedArray.push(obj)
+
+          }
+
+          function shuffle(a) {
+              var j, x, i;
+                for (i = a.length - 1; i > 0; i--) {
+                    j = Math.floor(Math.random() * (i + 1));
+                    x = a[i];
+                    a[i] = a[j];
+                    a[j] = x;
+                }
+                return a;
+          }
+
+            // randomize the array struct
+          shuffle(matchedArray)
+
+          $el.gameData = matchedArray
+
+      });
+  },
+  methods: {
+      nextQA() {
+          this.stage = this.stage + 1
+          this.showNextButton = false
+      },
+      showControls(value) {
+          this.showNextButton = value
+      }
   }
 };
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Rubik', sans-serif;
+  font-weight: 300;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #212121;
+  padding: 10vh 10vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+}
+.game-name {
+    position: absolute;
+    top: 1em;
+    left: 1em;
+    right: 1em;
+    text-align: center;
+    font-weight: 600;
+    opacity: 0.25;
+}
+
+.buttons {
+    margin-top: 3rem;
+}
+
+button {
+    border: none;
+    outline: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+
+    position: relative;
+    display: inline-flex;
+    align-items: center;
+    margin: 0;
+    padding: 1.25em 1em;
+    font-size: 1rem;
+    background: #ef4538;
+    color: #fff;
+    border-radius: 0.125em;
+    line-height: 1;
+    transition: 150ms ease;
+    text-decoration: none;
+    user-select: none;
+    cursor: pointer;
+    overflow: hidden;
+    font-family: inherit;
 }
 </style>
---
+++ src/components/QA.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="question-answer">
+        <div class="question">
+            <p>
+                {{ data['question'] }}
+            </p>
+        </div>
+        <div class="answer" :class="{ 'answer-hidden': !answerShown }">
+            <p>
+                {{ data['answer'] }}
+            </p>
+        </div>
+        <div class="buttons" v-if="!answerShown">
+            <button class="button" @click="showAnswer">Show Answer</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "QA",
+  props: {
+    data: Object,
+    stage: Boolean
+  },
+  data() {
+      return {
+          answerShown: false
+      }
+  },
+  methods: {
+      showAnswer() {
+          this.answerShown = !this.answerShown
+          this.$emit('stage', this.answerShown)
+      }
+  },
+  mounted() {
+      this.answerShown = false
+  },
+  watch: {
+      data() {
+          this.answerShown = false
+      }
+  }
+};
+</script>
+
+<style lang="scss">
+p {
+    margin: 0;
+}
+
+.question {
+    font-size: clamp(3rem, calc(1rem + 4vw), 5rem);
+}
+.answer {
+    margin-top: 3rem;
+    margin-bottom: 3rem;
+    color: #ef4538;
+    font-size: clamp(2rem, calc(1rem + 3vw), 3rem);
+    transition: 350ms cubic-bezier(0.34, 2, 0.64, 1);
+    transform: scale(1);
+    opacity: 1;
+
+    &-hidden {
+        opacity: 0;
+        transform: scale(0);
+        color: #000;
+        transition: 0s ease;
+    }
+}
+</style>
