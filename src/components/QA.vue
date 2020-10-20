eb8818e593d9325da35b03d4c3b98fbcac8913a6
Kolby Kruger <kolbykruger@gmail.com>
2020-10-20 11:06:53 -0400

    building out choices

--- src/components/QA.vue
+++ src/components/QA.vue
@@ -15,6 +15,21 @@
                 {{ data['person'].text }}
             </p>
         </div>
+        <div class="choices">
+            <button
+                class="choice"
+                v-for="name in choices"
+                :key="name.id"
+                @click="choice(name)"
+                :class="isChosen(name)"
+                :title="name"
+            >
+                <img :src="name + '.png'" />
+            </button>
+            <br />
+            <br />
+            {{ picks }} guesses remaining
+        </div>
         <div class="buttons" v-if="!answerShown">
             <button class="button" @click="showAnswer">Show Answer</button>
         </div>
@@ -31,22 +46,61 @@ export default {
     data() {
         return {
             answerShown: false,
-            ready: false
+            ready: false,
+            picks: 2,
+            chosen: [],
+            choices: [
+                'Mike',
+                'Rob',
+                'Carlos',
+                'Kolby',
+                'Meghan',
+                'Shawna',
+                'Grace'
+            ]
         }
     },
+    computed: {},
     methods: {
         showAnswer() {
-            this.answerShown = !this.answerShown
+            this.answerShown = true
             this.$emit('stage', this.answerShown)
+        },
+        choice(name) {
+            //if (this.picks)
+            if (name == this.data['person'].text) {
+                console.log('Correct!')
+                this.showAnswer()
+            } else {
+                console.log('Incorrect')
+            }
+
+            this.chosen.push(name)
+            this.picks = this.picks - 1
+        },
+        isChosen(name) {
+            if (this.chosen.includes(name)) {
+                return 'choice-chosen'
+            }
         }
     },
     mounted() {
         this.answerShown = false
         this.ready = true
+        this.picks = 2
+        this.chosen = []
     },
     watch: {
         data() {
             this.answerShown = false
+            this.picks = 2
+            this.ready = true
+            this.chosen = []
+        },
+        picks() {
+            if (this.picks == 0) {
+                this.showAnswer()
+            }
         }
     }
 }
@@ -83,6 +137,36 @@ h2,
         transform: scale(0);
         color: #000;
         transition: 0s ease;
+    }
+}
+
+.choices {
+    .choice {
+        appearance: none;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        background: none;
+        border: none;
+        outline: none;
+        font-family: inherit;
+        font-size: 1.25rem;
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+        padding: 0;
+        margin: 0 0.25em;
+        overflow: hidden;
+        cursor: pointer;
+
+        img {
+            object-fit: cover;
+            width: 100%;
+            height: 100%;
+        }
+
+        &-chosen {
+            opacity: 0.2;
+        }
     }
 }
 </style>
